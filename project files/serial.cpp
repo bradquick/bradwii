49974ad576ef08ff26effc7d70549846a744702f
Brad Quick <brad@jamesltaylor.com>
2013-07-07 11:08:05 -0400

    Changed version to 1.02

--- project files/serial.cpp
+++ project files/serial.cpp
@@ -36,7 +36,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "gps.h"
 
 #define MSP_VERSION 0
-#define  VERSION  101 // version 1.01
+#define  VERSION  102 // version 1.02
 
 
 extern globalstruct global;
