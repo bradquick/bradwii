fb838d9fb23e8f095e236f30571dfe0e1e75f261
Brad Quick <brad@jamesltaylor.com>
2013-08-08 17:34:59 -0400

    Set version

--- project files/serial.cpp
+++ project files/serial.cpp
@@ -36,7 +36,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "gps.h"
 
 #define MSP_VERSION 0
-#define  VERSION  108 // version 1.08
+#define  VERSION  110 // version 1.10
 
 
 extern globalstruct global;
